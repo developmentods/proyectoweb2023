2a3617b54d0d2b349e47563df020b7f7faac3b05
Josep Aguilar <jaguilar315@unab.edu.co>
2023-10-04 21:13:11 -0500

    config bd para railway

--- Config/database.php
+++ Config/database.php
@@ -2,10 +2,10 @@
     class Connect{
 
         public static function Conectar(){
-        $servidor = "localhost";
-        $db = "biblioteca";
+        $servidor = "containers-us-west-86.railway.app";
+        $db = "railway";
         $user = "root";
-        $pass = "admin1234";
+        $pass = "piYf9Jpa2turlzlyMLcu";
 
             $conexion = new PDO("mysql:host=$servidor;dbname=$db;",$user,$pass);
             
